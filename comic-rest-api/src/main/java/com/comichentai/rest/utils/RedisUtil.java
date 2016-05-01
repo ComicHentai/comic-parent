d837ef7d1672aef5691ce2dd13aff456c36fc79f
dintama <dingyi6680@qq.com>
2016-05-01 15:24:06 +0800

    redis resource

---
+++ comic-rest-api/src/main/java/com/comichentai/rest/utils/RedisUtil.java
@@ -0,0 +1,21 @@
+package com.comichentai.rest.utils;
+
+import org.springframework.data.redis.core.RedisTemplate;
+
+import javax.annotation.Resource;
+
+/**
+ * Created by dintama on 16/4/3.
+ */
+public class RedisUtil {
+
+    @Resource(name = "redisTemplate")
+    private RedisTemplate<String, Object> redisTemplate;
+
+
+    public String get(String key){
+        String result = (String)redisTemplate.opsForValue().get(key);
+        return result;
+    }
+
+}
