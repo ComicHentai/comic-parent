bed28ca3b37070fe9a6be139e3ffffa3c10dc2b5
hope6537 <hope6537@qq.com>
2016-05-12 06:47:57 +0800

    [change mode]

---
+++ deploy-rest.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+echo "deploying restapi"
+mvn clean install -Dmaven.test.skip;
+cd comic-rest-api;
+mvn clean install -Dmaven.test.skip;
+nohup java -jar target/comic-rest-api-1.3.2.RELEASE &;
--- deploy-service.sh
+++ deploy-service.sh
@@ -6,7 +6,7 @@ mvn clean install -Dmaven.test.skip;
 cd target;
 tar -xvzf comic-deploy.tar.gz
 cd comic-deploy;
-./bin/start_low_mem.sh;
+sh ./bin/start_low_mem.sh;
 
 cd ../../;
 cd comic-rest-api
