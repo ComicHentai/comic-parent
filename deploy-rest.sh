375c7e19ab86e16873164417e89790be3559c404
hope6537 <hope6537@qq.com>
2016-05-12 07:05:13 +0800

    [change mode]

--- deploy-rest.sh
+++ deploy-rest.sh
@@ -4,6 +4,6 @@ echo "deploying restapi"
 mvn clean install -Dmaven.test.skip;
 cd comic-rest-api;
 mvn clean install -Dmaven.test.skip;
-nohup java -jar target/comic-rest-api-1.3.2.RELEASE &
+nohup java -jar target/comic-rest-api-1.3.2.RELEASE.jar &
 
 tail -f nohup.out;
